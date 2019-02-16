e8f6a9c886537f91bdbefb3eb49b4637a9c036c8
Dale Bingham <dalebingham@Dales-Main-Macbook.local>
2019-02-16 08:59:33 -0500

    Removing non read-only code. Fixing InternalId as the key

--- Data/ArtifactRepository.cs
+++ Data/ArtifactRepository.cs
@@ -35,13 +35,22 @@ namespace openstig_read_api.Data {
             }
         }
 
+        private ObjectId GetInternalId(string id)
+        {
+            ObjectId internalId;
+            if (!ObjectId.TryParse(id, out internalId))
+                internalId = ObjectId.Empty;
+
+            return internalId;
+        }
+
         // query after Id or InternalId (BSonId value)
         //
         public async Task<Artifact> GetArtifact(string id)
         {
             try
             {
-                return await _context.Artifacts.Find(artifact => artifact.id == new Guid(id)).FirstOrDefaultAsync();
+                return await _context.Artifacts.Find(artifact => artifact.InternalId == GetInternalId(id)).FirstOrDefaultAsync();
             }
             catch (Exception ex)
             {
@@ -68,75 +77,6 @@ namespace openstig_read_api.Data {
             }
         }
         
-        public async Task AddArtifact(Artifact item)
-        {
-            try
-            {
-                await _context.Artifacts.InsertOneAsync(item);
-            }
-            catch (Exception ex)
-            {
-                // log or manage the exception
-                throw ex;
-            }
-        }
-
-        public async Task<bool> RemoveArtifact(string id)
-        {
-            try
-            {
-                DeleteResult actionResult 
-                    = await _context.Artifacts.DeleteOneAsync(
-                        Builders<Artifact>.Filter.Eq("Id", id));
-
-                return actionResult.IsAcknowledged 
-                    && actionResult.DeletedCount > 0;
-            }
-            catch (Exception ex)
-            {
-                // log or manage the exception
-                throw ex;
-            }
-        }
-
-        public async Task<bool> UpdateArtifact(string id, Artifact body)
-        {
-            var filter = Builders<Artifact>.Filter.Eq(s => s.id.ToString(), id);
-            var update = Builders<Artifact>.Update
-                            .Set(s => s, body)
-                            .CurrentDate(s => s.updatedOn);
-
-            try
-            {
-                UpdateResult actionResult 
-                    = await _context.Artifacts.UpdateOneAsync(filter, update);
-
-                return actionResult.IsAcknowledged
-                    && actionResult.ModifiedCount > 0;
-            }
-            catch (Exception ex)
-            {
-                // log or manage the exception
-                throw ex;
-            }
-        }
-
-        public async Task<bool> RemoveAllArtifacts()
-        {
-            try
-            {
-                DeleteResult actionResult 
-                    = await _context.Artifacts.DeleteManyAsync(new BsonDocument());
-
-                return actionResult.IsAcknowledged
-                    && actionResult.DeletedCount > 0;
-            }
-            catch (Exception ex)
-            {
-                // log or manage the exception
-                throw ex;
-            }
-        }
         public async Task<long> CountChecklists(){
             try {
                 long result = await _context.Artifacts.CountDocumentsAsync(Builders<Artifact>.Filter.Empty);
