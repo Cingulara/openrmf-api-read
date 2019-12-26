e0307b1088b9d85911b7123d00e0151df1be306d
Dale Bingham <dalebingham@Dales-Main-Macbook.local>
2019-12-26 17:55:48 -0500

    updating copyright notice missing

--- src/Models/STIGS.cs
+++ src/Models/STIGS.cs
@@ -1,5 +1,5 @@
-using System;
-using System.Collections.Generic;
+// Copyright (c) Cingulara 2019. All rights reserved.
+// Licensed under the GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007 license. See LICENSE file in the project root for full license information.
 
 namespace openrmf_read_api.Models
 {
