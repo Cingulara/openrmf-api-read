a187ad6b5ea04d11400fc5d52a252c878431062f
DaleBinghamSoteriaSoft <dale.bingham@soteriasoft.com>
2022-05-16 14:06:21 -0400

    update to dotnet 6 and v1.8

--- src/Startup.cs
+++ src/Startup.cs
@@ -73,7 +73,7 @@ namespace openrmf_read_api
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "OpenRMF Read API", Version = "v1", 
-                    Description = "The Read API that goes with the OpenRMF tool",
+                    Description = "The Read API that goes with the OpenRMF OSS Application",
                     Contact = new OpenApiContact
                     {
                         Name = "Dale Bingham",
@@ -118,6 +118,7 @@ namespace openrmf_read_api
             services.AddAuthorization(options =>
             {
                 options.AddPolicy("Administrator", policy => policy.RequireRole("roles", "[Administrator]"));
+                options.AddPolicy("Download", policy => policy.RequireRole("roles", "[Download]"));
                 options.AddPolicy("Editor", policy => policy.RequireRole("roles", "[Editor]"));
                 options.AddPolicy("Reader", policy => policy.RequireRole("roles", "[Reader]"));
                 options.AddPolicy("Assessor", policy => policy.RequireRole("roles", "[Assessor]"));
