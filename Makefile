044353665961f09c3bb430d18ac03c54a097d9f6
Dale Bingham <dale.bingham@soteriasoft.com>
2025-12-02 10:38:39 -0500

    update the JWT Auth code

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION ?= 1.14.00
+VERSION ?= 1.14.01
 NAME ?= "openrmf-api-read"
 AUTHOR ?= "Dale Bingham"
 PORT_EXT ?= 8084
--- src/Startup.cs
+++ src/Startup.cs
@@ -120,9 +120,9 @@ namespace openrmf_read_api
 
             foreach(string jwtServer in Environment.GetEnvironmentVariable("JWTAUTHORITY").Split(',')) {
                 if (jwtServer.EndsWith('/'))
-                    jwtAuthorities.Add((jwtServer + "realms/openrmf").Trim().ToLower());
+                    jwtAuthorities.Add((jwtServer).Trim().ToLower());
                 else 
-                    jwtAuthorities.Add((jwtServer + "/realms/openrmf").Trim().ToLower());
+                    jwtAuthorities.Add((jwtServer + "/").Trim().ToLower());
             }
 
             if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("JWTINTERNALAUTHORITY"))) {
